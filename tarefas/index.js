fbca7eb8ec507df93052bd54588b7beed4a7f03a
Keven <kevenleonardo11@gmail.com>
2024-04-22 19:11:50 -0300

    atualizado

--- tarefas/index.js
+++ tarefas/index.js
@@ -1,31 +1,40 @@
-const express = require("express");
 
+const express = require("express");
 const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+const tarefa = require('./model/tarefa')
 
 // route
 // get -> receber dados
-// post -> enviar dados 
+// post -> enviar dados
 
-app.get("/listar-tarefas", function(req,res){
-    res.send("tarefas cadastradas");
+
+app.get("/listar-tarefas", function(req, res){
+
+    let tarefas = tarefa.listar();
+
+    res.json(tarefas);
 });
 
-app.post("/nova-tarefa",function(req,res){
+app.post("/nova-tarefa", function(req, res){
     res.send("tarefa adicionada");
 });
 
-app.post("/alterar-tarefa", function(req,res){
-    res.send("tarefa alterada");
+app.post("/alterar-tarefa", function(req, res) {
+    res.send("tarefa alterada")
 });
 
-app.post("deletar-tarefa", function(req,res){
+app.post("/deletar-tarefa", function(req, res){
     res.send("tarefa deletada");
 });
 
-app.post("/logar", function(req,res){
+app.post("/logar", function(req, res){
     res.send("usuario logado");
-});
+})
 
 app.listen(8000, function(){
-    console.log("servidor iniciado");
+    console.log("Servidor iniciado")
 });
--- tarefas/model/tarefa.js
+++ tarefas/model/tarefa.js
@@ -1,16 +1,16 @@
-/** 
- * Adicionar a tarefa ao BD
- * @param {*} nova Dados da nova tarefa
- */
 
+/**
+ * Adicionar a tarefa ao BD
+ * @param {*} nova Dados da nova tarefa 
+ */
 function cadastrar(nova)
 {
-    
+
 }
 
 /**
  * Seta o status da tarefa como concluido
- * @param {*} tarefa Tarefa selecionada
+ * @param {*} tarefa Tarefa selecionada 
  */
 function concluir(tarefa)
 {
@@ -27,7 +27,7 @@ function modificar(tarefa, novoDados)
 }
 
 /**
- * Remove a tarefa do banco de BD
+ * Remove a tarefa do BD
  * @param {*} tarefa Tarefa selecionada
  */
 function excluir(tarefa)
@@ -39,7 +39,7 @@ function excluir(tarefa)
  * Altera o status para o valor informado
  * @param {*} tarefa Tarefa selecionada
  */
-function status()
+function status(tarefa, status)
 {
 
 }
@@ -53,11 +53,41 @@ function data(tarefa, data)
     
 }
 
+/**
+ * Lista tarefas cadastradas
+ */
+function listar()
+{
+    let tarefas = [
+        {
+            concluido: false,
+            titulo: "Fazer dever de casa",
+            data_limite: "2024-04-20",
+            data_criado: "2024-04-18"
+        },
+        {
+            concluido: true,
+            titulo: "Ir no dentista",
+            data_limite: "2024-04-20",
+            data_criado: "2024-04-18"
+        },
+        {
+            concluido: false,
+            titulo: "Lavar roupa",
+            data_limite: "2024-04-20",
+            data_criado: "2024-04-18"
+        }
+    ];
+
+    return tarefas;
+}
+
 module.exports = {
     cadastrar,
     concluir,
     modificar,
     excluir,
     status,
-    data
+    data,
+    listar
 }
